f659d5045dcf713cd7290563ad1399eee873f883
IsuruRaveen20 <isururaveen4520@gmail.com>
2023-04-05 22:45:59 +0530

    Api intergated

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+        'X-RapidAPI-Key': '3826c894d6msh1dd416701d6c374p16e2dejsn4dcb2f0af20a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo/';
